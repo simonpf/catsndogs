c5f681008b0d6a131fcf6a009033f4942a98231f
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-05-06 07:28:16 +0200

    Added appveyor setup.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='catsndogs',  # Required
-    version='0.0.4',  # Required
+    version='0.0.5',  # Required
     description='A dataset containing cats and dogs.',
     long_description=long_description,
     long_description_content_type='text/markdown',  # Optional (see note above)
     url='https://github.com/simonpf/catsndogs',  # Optional
     author='Simon Pfreundschuh',  # Optional
     author_email='simon.pfreundschuh@chalmers.se',  # Optional
-    install_requires=["appdirs", "requests", "matplotlib"],
+    install_requires=["appdirs", "requests"],
     packages=["catsndogs"],
     python_requires='>=3.6',
     project_urls={  # Optional
--- test/test_download.py
+++ test/test_download.py
@@ -7,8 +7,13 @@ import os
 def test_test_data():
     folders = glob.glob(os.path.join(catsndogs.test.folder, "*"))
     assert(len(folders) == 2)
+    assert(len(catsndogs.test.cats) == 239)
+    assert(len(catsndogs.test.dogs) == 500)
 
 def test_training_data_data():
     folders = glob.glob(os.path.join(catsndogs.training.folder, "*"))
     assert(len(folders) == 2)
+    assert(len(catsndogs.training.cats) == 949)
+    assert(len(catsndogs.training.dogs) == 1998)
+
 
